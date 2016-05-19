d4b7801a1a0e2a0c1d8c53f882db91403c9c5099
Erich Abellera <ErichAbellera@Erich-Abelleras-MacBook-Pro.local>
2016-05-19 12:41:16 -0700

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=25496b516f6b713fc9c841854ba19cfd&units=imperial';
+
+//25496b516f6b713fc9c841854ba19cfd
+module.exports = {
+    getTemp: function(location){
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+        
+        return axios.get(requestUrl).then(function(res){
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message);
+            }
+            else {
+                return res.data.main.temp;
+            }
+        }, function(res){
+            throw new Error(res.data.message);
+        });
+    }
+}
